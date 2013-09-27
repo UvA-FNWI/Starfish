757afa2e1482b6933ca35eaf52de7b05ad536b56
mslatour <sanderlatour@gmail.com>
2013-09-27 14:23:33 +0200

    Added more info in populate

--- populate.py
+++ populate.py
@@ -39,6 +39,18 @@ def populate_persons():
         linkbase+"e.a.van-vliet.html?f=Vliet",
         "E.A.vanVliet@uva.nl")
 
-    LearningAnalytics = ensure_tag('O', "LearningAnalytics")
-    NatasaBrouwer.tags.add(LearningAnalytics)
+    t1 = ensure_tag('O', "LearningAnalytics")
+    NatasaBrouwer.tags.add(t1)
+    AndreHeck.tags.add(t1)
+    t2 = ensure_tag('O','Toetsen en toetsgestuurd leren')
+    NatasaBrouwer.tags.add(t2)
+    t3 = ensure_tag('O', "Afstand onderwijs en zelfstandig leren")
+    NatasaBrouwer.tags.add(t3)
+    t4 = ensure_tag('O', "Nieuwe Onderwijsconcepten")
+    NatasaBrouwer.tags.add(t4)
+    t5 = ensure_tag('T', "Blackboard")
+    NatasaBrouwer.tags.add(t5)
+    t6 = ensure_tag('T', "Stemkasjes")
+    NatasaBrouwer.tags.add(t6)
+    ErwinVanVliet.tags.add(t6)
 
