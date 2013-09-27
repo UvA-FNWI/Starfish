fc88e6ee75a19723ec66b9cb0c5db4d51b56e23f
Chiel Kooijman <chiel999@gmail.com>
2013-09-27 17:42:09 +0200

    hack to remove django errors in ipython.

--- steep/settings.py
+++ steep/settings.py
@@ -9,6 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# Ignore the following error when using ipython:
+import warnings
+import exceptions
+warnings.filterwarnings("ignore", category=exceptions.RuntimeWarning,
+        module='django.db.backends.sqlite3.base', lineno=53)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
