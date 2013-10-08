2600ec4a587c76384cb6c9b2f303dfda9fd864c9
mslatour <sanderlatour@gmail.com>
2013-10-08 02:33:58 +0200

    Created queryset of tags that can be aliased

--- search/admin.py
+++ search/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from models import *
 
+class TagAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "alias_of":
+            kwargs["queryset"] = Tag.objects.filter(alias_of=None)
+        return super(TagAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 admin.site.register(Person)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(GoodPractice)
 admin.site.register(Information)
 admin.site.register(Project)
