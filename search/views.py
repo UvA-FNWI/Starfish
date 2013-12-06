482dc5c164bd836611552ac7876e2f74b5551d06
Auke <wiggers.auke@gmail.com>
2013-12-06 14:23:25 +0100

    Another attempt at solving broken pipe. Ajax used to validate forms, but unable to parse response correctly.

--- search/views.py
+++ search/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from django.views import generic
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext, loader
+from django.core import serializers
 
 from search.models import *
 from search.forms import *
@@ -242,46 +243,47 @@ def vote(request, model_type, model_id, vote):
         # TODO redirect, show already voted / undo vote
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
-def validatequestion(request):
+def submitquestion(request):
     return askquestion(request)
 
 @login_required(login_url='/login/')
-def askquestion(request):
+def loadquestion(request):
     item_type = request.GET.get('type', '')
     item_id = int(request.GET.get('id', ''))
     item = get_model_by_sub_id(item_type, item_id)
 
-    if request.method == "POST":
-        questionform = QuestionForm(request.POST)
-        logger.debug("request is POST")
-        if questionform.is_valid():
-            logger.debug('questionform valid')
-            item_type = questionform.cleaned_data['item_type']
-            item_id = questionform.cleaned_data['item_id']
-
-            question = questionform.save(commit=False)
-            # TODO get current author
-            logger.debug("Question submitted by user '{}'".format(request.user))
-            question.author = Person.objects.filter(name__istartswith="Nat")[0]
-            question.save()
-            questionform.save_m2m()
-
-            if item:
-                item.links.add(question)
-                question.links.add(item)
-            data = {'success': True,
-                    'redirect': question.get_absolute_url(),
-                    }
-            return HttpResponse(json.dumps(data))
-        else:
-            logger.debug("questionform invalid")
-            return render(request, 'askquestion.html', {'form': questionform})
-    else:
-        logger.debug("initial questionform")
-        questionform = QuestionForm(initial={'item_type': item_type,
-                                             'item_id': item_id})
+    logger.debug("initial questionform")
+    questionform = QuestionForm(initial={'item_type': item_type,
+                                         'item_id': item_id})
     return render(request, 'askquestion.html', {'form': questionform})
 
+def submitquestion(request):
+    logger.debug(request)
+    if request.method == "POST":
+        if request.is_ajax():
+            questionform = QuestionForm(request.POST)
+            logger.debug("request is POST")
+            if questionform.is_valid():
+                logger.debug('questionform valid')
+                item_type = questionform.cleaned_data['item_type']
+                item_id = questionform.cleaned_data['item_id']
+                question = questionform.save(commit=False)
+                # TODO get current author
+                logger.debug("Question submitted by user '{}'".format(request.user))
+                question.author = Person.objects.filter(name__istartswith="Nat")[0]
+                question.save()
+                questionform.save_m2m()
+
+                if item:
+                    item.links.add(question)
+                    question.links.add(item)
+                data = serializers.serialize('json', [question])
+            else:
+                logger.debug("questionform invalid")
+                data = json.dumps({'errors': dict([(k, [unicode(e) for e in v])
+                                   for k,v in questionform.errors.items()])})
+            return HttpResponseBadRequest(data, mimetype='application/json')
+    return HttpResponseBadRequest()
 
 @login_required(login_url='/login/')
 def comment(request):
