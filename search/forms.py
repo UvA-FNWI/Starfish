2cb22473334c8512bd0f3f3490690c295451d230
Chiel Kooijman <chiel999@gmail.com>
2014-03-16 12:32:03 +0100

    Dashboard Forms - Create new and Edit existing

--- search/forms.py
+++ search/forms.py
@@ -43,44 +43,63 @@ class QuestionForm(ModelForm):
 #        super(EditForm, self).__init__(*args, **kwargs)
 
 
-class EditInformationForm(ModelForm):
+class DashboardForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(DashboardForm, self).__init__(*args, **kwargs)
+        self.fields['tags'].widget = TagInput()
+        self.fields['tags'].help_text = None
+
+
+class EditInformationForm(DashboardForm):
     class Meta:
         model = Information
-        fields = ['title', 'text', 'links']
+        fields = ['title', 'text', 'links', 'author', 'communities', 'tags',
+                  'links']
 
 
-class EditCommentForm(ModelForm):
+class EditCommentForm(DashboardForm):
     class Meta:
         model = Information
         fields = ['title', 'text', 'tags']
 
 
-class EditGoodPracticeForm(ModelForm):
+class EditGoodPracticeForm(DashboardForm):
     class Meta:
-        model = Information
-        fields = ['title', 'text']
+        model = GoodPractice
+        fields = ['title', 'text', 'links', 'author', 'communities', 'tags',
+                  'links']
 
 
-class EditPersonForm(ModelForm):
+class EditQuestionForm(DashboardForm):
+    class Meta:
+        model = GoodPractice
+        fields = ['title', 'text', 'links', 'author', 'communities', 'tags',
+                  'links']
+
+
+class EditPersonForm(DashboardForm):
     class Meta:
         model = Person
         fields = ['title', 'name', 'headline', 'about', 'photo', 'website',
-                  'email']
+                  'email', 'communities']
 
 
-class EditProjectForm(ModelForm):
+class EditProjectForm(DashboardForm):
     class Meta:
         model = Project
-        fields = ['title', 'text', 'contact']
+        fields = ['title', 'text', 'contact', 'author', 'communities', 'links',
+                  'tags']
 
 
-class EditEventForm(ModelForm):
+class EditEventForm(DashboardForm):
     class Meta:
         model = Event
-        fields = ['title', 'text', 'contact']
+        fields = ['title', 'text', 'contact', 'author', 'communities', 'links',
+                  'tags', 'date']
 
 
-class EditGlossaryForm(ModelForm):
+class EditGlossaryForm(DashboardForm):
     class Meta:
         model = Event
-        fields = ['title', 'text']
+        fields = ['title', 'text', 'tags', 'links', 'communities']
